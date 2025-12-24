85486918001679781d536e9e291cb20e5f073ae5
Parszuto <pedrohenriqueparszutodacruz@gmail.com>
2025-12-24 14:43:17 -0300

    started-new-project

---
+++ casa-do-hamburguer/front-end/src/Header.tsx
@@ -0,0 +1,12 @@
+const Header = () => {
+    return <div className="bg-[#161410]">
+        <div className="w-full md:w-[737px] mx-auto p-3px md:p-0 flex justify-between items-center">
+        <img src="./logo.png" alt="Hamburguer" />
+        <div className="bg-[#F2DAAC] flex items-center justify-center w-[130px] h-[35px] rounded-sm ">
+            Entrar
+        </div>
+        </div>
+        </div>
+}
+
+export default Header;
