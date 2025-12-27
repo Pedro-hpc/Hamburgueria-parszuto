1163a8b6ba50857a6b6b32b1fc95d91119e9be3e
Parszuto <pedrohenriqueparszutodacruz@gmail.com>
2025-12-26 21:25:17 -0300

    add body of project

--- casa-do-hamburguer/front-end/src/App.tsx
+++ casa-do-hamburguer/front-end/src/App.tsx
@@ -1,5 +1,12 @@
+import Input from "./components/Input";
+
 const App = () => {
-    return <div></div>
-}
+  return (
+    <div className="flex gap-2 bg-black p-6">
+      <Input placeholder="E-mail" type="text" />
+      <Input placeholder="Senha" type="password" />
+    </div>
+  );
+};
 
 export default App;
---
+++ casa-do-hamburguer/front-end/src/components/Header.tsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <div className="bg-[#161410]">
+      <div className="p-3px mx-auto flex w-full items-center justify-between md:w-184.25 md:p-0">
+        <img src="./logo.png" alt="Hamburguer" />
+        <div className="flex h-8.75 w-32.5 items-center justify-center rounded-sm bg-[#F2DAAC]">
+          Entrar
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
